a0e06dc5190cb40501360b19f32a2ce3dd3db19f
prem <premkumardanav@gmail.com>
2024-03-28 22:31:29 +0530

    changes folder structure and learnt about use client

--- blog/src/app/page.js
+++ blog/src/app/page.js
@@ -1,11 +1,12 @@
+'use client'
 import styles from "./page.module.css";
-
 export default function Home() {
   return (
     <main className={styles.main}>
-      <User name={"Prem Danav"} />
-      <User name={"John Doe"} />
+      {/* <User name={"Prem Danav"} />
+      <User name={"John Doe"} /> */}
       <h1>Home Page</h1>
+      <button onClick={() => alert("Hello")}>Click Me</button>
     </main>
   );
 }
